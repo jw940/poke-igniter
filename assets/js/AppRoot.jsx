73d77f6307f0261cdc61f7afacdaa0ee24323ec1
JoeWhite940 <joewhite94@hotmail.com>
2017-10-07 17:15:29 +0100

    Example of successful api fetch + update response to state

--- assets/js/AppRoot.jsx
+++ assets/js/AppRoot.jsx
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {AppActions} from './AppState';
 
 class AppRoot extends Component {
+
+    componentDidMount() {
+        fetch("https://pokeapi.co/api/v2/pokemon/1/", {
+            method:"GET",
+            mode: "cors",
+        })
+        .then(res => {
+            return res.json();
+        })
+        .then(json => {
+            this.props.setPokemonDisplay(json);
+        })
+    }
+    
     render() {
+        if (this.props.pokemon.name) {
         return (
             <div>
-                <h1>{this.props.title}</h1>
+                <h1>{this.props.pokemon.name}</h1>
             </div>
         );
+        } else {
+            return (
+                <div>
+                    Loading..
+                </div>
+            )
+        }
     }
 }
 
 const mapStateToProps = state => {
     return {
-        title: state.title
+        pokemon: state.pokemon
     }
 }
 
-export default connect(mapStateToProps)(AppRoot);
+const mapDispatchToProps = dispatch => {
+    return {
+        setPokemonDisplay: pokemon => {
+            dispatch(AppActions.SetPokemonDisplay(pokemon));
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppRoot);
--- assets/js/AppState.js
+++ assets/js/AppState.js
@@ -1,9 +1,23 @@
 export const defaultState = {
-    title: "hello world"
+    pokemon: {}
 };
+
+export const AppActions = {
+    SetPokemonDisplay: pokemon => {
+        return {
+            type: "SET_POKEMON_DISPLAY",
+            pokemon: pokemon
+        }
+    }
+}
 
 export default function AppState(state, action) {
     switch(action.type) {
+
+        case "SET_POKEMON_DISPLAY":
+            let newState = Object.assign({}, state);
+            newState.pokemon = action.pokemon;
+            return newState
 
         default:
             return state
